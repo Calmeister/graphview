6b4e5db208ef707cb7c5b74fa2ce69c2fbd94db0
Austin Cauley <acauley@uco.edu>
2020-11-06 10:43:10 -0600

    More safety

--- lib/edgerenderer/ArrowEdgeRenderer.dart
+++ lib/edgerenderer/ArrowEdgeRenderer.dart
@@ -1,7 +1,7 @@
 part of graphview;
 
 const double ARROW_DEGREES = 0.5;
-const double ARROW_LENGTH = 100;
+const double ARROW_LENGTH = 20;
 
 class ArrowEdgeRenderer extends EdgeRenderer {
   var trianglePath = Path();
