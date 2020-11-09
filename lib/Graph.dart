217c3e688859a4450e084a2fb487b8dae59d03c3
Austin Cauley <clikok@aol.com>
2020-11-09 07:56:15 -0600

    Let's get spooky

--- lib/Graph.dart
+++ lib/Graph.dart
@@ -32,15 +32,18 @@ class Graph {
 
     _nodes.remove(node);
 
-    _edges.removeWhere((edge) => edge.source == node || edge.destination == node);
+    _edges
+        .removeWhere((edge) => edge.source == node || edge.destination == node);
 
     notifyGraphObserver();
   }
 
   void removeNodes(List<Node> nodes) => nodes.forEach((it) => removeNode(it));
 
-  Edge addEdge(Node source, Node destination, {Paint paint}) {
-    final edge = Edge(source, destination, paint: paint);
+  Edge addEdge(Node source, Node destination,
+      {Paint paint, Widget embeddedWidget}) {
+    final edge =
+        Edge(source, destination, paint: paint, embeddedWidget: embeddedWidget);
     addEdgeS(edge);
 
     return edge;
@@ -53,7 +56,8 @@ class Graph {
       _nodes.add(edge.source);
     }
     if (_nodes.contains(edge.destination)) {
-      edge.destination = _nodes.firstWhere((element) => element == edge.destination);
+      edge.destination =
+          _nodes.firstWhere((element) => element == edge.destination);
     } else {
       _nodes.add(edge.destination);
     }
@@ -71,26 +75,35 @@ class Graph {
   void removeEdges(List<Edge> edges) => edges.forEach((it) => removeEdge(it));
 
   void removeEdgeFromPredecessor(Node predecessor, Node current) {
-    _edges.removeWhere((edge) => edge.source == predecessor && edge.destination == current);
+    _edges.removeWhere(
+        (edge) => edge.source == predecessor && edge.destination == current);
   }
 
   bool hasNodes() => _nodes.isNotEmpty;
 
   Edge getEdgeBetween(Node source, Node destination) =>
-      _edges.firstWhere((element) => element.source == source && element.destination == destination);
+      _edges.firstWhere((element) =>
+          element.source == source && element.destination == destination);
 
-  bool hasSuccessor(Node node) => _edges.any((element) => element.source == node);
+  bool hasSuccessor(Node node) =>
+      _edges.any((element) => element.source == node);
 
-  List<Node> successorsOf(Node node) =>
-      _edges.where((element) => element.source == node).map((e) => e.destination).toList();
+  List<Node> successorsOf(Node node) => _edges
+      .where((element) => element.source == node)
+      .map((e) => e.destination)
+      .toList();
 
-  bool hasPredecessor(Node node) => _edges.any((element) => element.destination == node);
+  bool hasPredecessor(Node node) =>
+      _edges.any((element) => element.destination == node);
 
-  List<Node> predecessorsOf(Node node) =>
-      _edges.where((element) => element.destination == node).map((edge) => edge.source).toList();
+  List<Node> predecessorsOf(Node node) => _edges
+      .where((element) => element.destination == node)
+      .map((edge) => edge.source)
+      .toList();
 
   bool contains({Node node, Edge edge}) =>
-      node != null && _nodes.contains(node) || edge != null && _edges.contains(edge);
+      node != null && _nodes.contains(node) ||
+      edge != null && _edges.contains(edge);
 
 //  bool contains(Edge edge) => _edges.contains(edge);
 
@@ -109,13 +122,17 @@ class Graph {
     return _nodes[position];
   }
 
-  Node getNodeAtUsingData(Widget data) => _nodes.firstWhere((element) => element.data == data);
+  Node getNodeAtUsingData(Widget data) =>
+      _nodes.firstWhere((element) => element.data == data);
 
-  Node getNodeUsingKey(Key key) => _nodes.firstWhere((element) => element.key == key);
+  Node getNodeUsingKey(Key key) =>
+      _nodes.firstWhere((element) => element.key == key);
 
-  List<Edge> getOutEdges(Node node) => _edges.where((element) => element.source == node).toList();
+  List<Edge> getOutEdges(Node node) =>
+      _edges.where((element) => element.source == node).toList();
 
-  List<Edge> getInEdges(Node node) => _edges.where((element) => element.destination == node).toList();
+  List<Edge> getInEdges(Node node) =>
+      _edges.where((element) => element.destination == node).toList();
 
   void notifyGraphObserver() => graphObserver.forEach((element) {
         element.notifyGraphInvalidated();
@@ -151,7 +168,8 @@ class Node {
   }
 
   @override
-  bool operator ==(Object other) => identical(this, other) || other is Node && hashCode == other.hashCode;
+  bool operator ==(Object other) =>
+      identical(this, other) || other is Node && hashCode == other.hashCode;
 
   @override
   int get hashCode => key?.hashCode ?? data.hashCode;
@@ -170,10 +188,12 @@ class Edge {
   Key key;
   Paint paint;
 
-  Edge(this.source, this.destination, {this.key, this.paint, this.embeddedWidget});
+  Edge(this.source, this.destination,
+      {this.key, this.paint, this.embeddedWidget});
 
   @override
-  bool operator ==(Object other) => identical(this, other) || other is Edge && hashCode == other.hashCode;
+  bool operator ==(Object other) =>
+      identical(this, other) || other is Edge && hashCode == other.hashCode;
 
   @override
   int get hashCode => key?.hashCode ?? source.hashCode ^ destination.hashCode;
